0e0a80802f685b4f2581a468c27534aaf419baff
ezio84 <brabus84@gmail.com>
2015-03-25 17:57:30 +0100

    Custom System Tools Menu in Settings
    System Tools will have the stock slimcenter icon, so it can be
    themed by a common overlay. The Slimcenter entry will have an
    alternative default color (1F444C) so if no overlays are set,
    the Slimcenter icon has a different color from the System Tools
    icon.
    
    Change-Id: Ia19c01292a0b5a0aa1a3ac5241f8d7d88a1b3843
    
    Conflicts:
    	res/xml/dashboard_categories.xml
    
    Show an AdAway entry in the System Tools menu and hide from drawer
    
    if the app is installed
    
    thanks to @letmedanz for the icons
    
    Change-Id: Id0d57595837b108a50639d8ed75aed9263a599d8
    
    Conflicts:
    	src/com/android/settings/SettingsActivity.java
    
    Systemtools menu entries: change icons and fix their layout
    
    thanks to @rootSU for the supersu and slimcenter icons
    http://forum.xda-developers.com/android/themes/add-theme-material-icons-settings-t3044976
    
    Change-Id: I1cc53b666575680b14dc8d46f3967607656e1966
    
    Systemtools: change Supersu icon
    
    Change-Id: I96e7825e333fe0d904a24a9c3d1fa17a165b6165

---
+++ src/com/android/settings/slim/SystemTools.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2013 SlimRoms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.List;
+
+public class SystemTools extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+        private static final String KEY_ADAWAY_APP = "adaway_app";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.system_tools);
+
+            if (!isPackageInstalled("org.adaway")) {
+                PreferenceScreen screen = getPreferenceScreen();
+                Preference pref = getPreferenceManager().findPreference(KEY_ADAWAY_APP);
+                screen.removePreference(pref);
+                }
+    }
+
+    private boolean isPackageInstalled(String packageName) {
+        PackageManager pm = getPackageManager();
+        boolean installed = false;
+        try {
+           pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+           installed = true;
+        } catch (PackageManager.NameNotFoundException e) {
+           installed = false;
+        }
+        return installed;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
