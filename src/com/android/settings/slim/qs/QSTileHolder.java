e8ec498107387dbde05b36bffca8e6e227a1ac3c
ezio84 <brabus84@gmail.com>
2015-05-30 14:22:34 +0200

    QS: add enable/disable LED tile [1/2]
    Change-Id: I5a178f1dcf16183154abb9bc1d012d366e83faa2
    
    QS led tile: new vector icon thanks to Michele Amendolagine

--- src/com/android/settings/slim/qs/QSTileHolder.java
+++ src/com/android/settings/slim/qs/QSTileHolder.java
@@ -161,6 +161,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_expanded_desktop";
                 stringId = R.string.qs_expanded_desktop_tile;
                 break;
+            case QSConstants.TILE_LED:
+                resourceName = "ic_qs_led_on";
+                stringId = R.string.qs_tile_led;
+                break;
             default:
                 return null;
         }
