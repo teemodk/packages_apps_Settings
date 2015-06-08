987230dc8e9d3872f5e0c62290f42dae1052e52e
ezio84 <brabus84@gmail.com>
2015-06-08 14:19:59 +0200

    Move ime cursors setting to the navbar menu
    Change-Id: I8053279f951aca8444a390435d48c0dd38c5bb8e

--- src/com/android/settings/slim/NavbarSettings.java
+++ src/com/android/settings/slim/NavbarSettings.java
@@ -49,6 +49,7 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
     private static final String PREF_BUTTON = "navbar_button_settings";
     private static final String PREF_STYLE_DIMEN = "navbar_style_dimen_settings";
     private static final String PREF_NAVIGATION_BAR_CAN_MOVE = "navbar_can_move";
+    private static final String STATUS_BAR_IME_ARROWS = "status_bar_ime_arrows";
 
     private static final int DLG_NAVIGATION_WARNING = 0;
 
@@ -60,6 +61,7 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
     SwitchPreference mNavigationBarCanMove;
     PreferenceScreen mButtonPreference;
     PreferenceScreen mStyleDimenPreference;
+    SwitchPreference mStatusBarImeArrows;
 
     private SettingsObserver mSettingsObserver = new SettingsObserver(new Handler());
     private final class SettingsObserver extends ContentObserver {
@@ -109,6 +111,12 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
             prefs.removePreference(mNavigationBarCanMove);
             mNavigationBarCanMove = null;
         }
+
+        mStatusBarImeArrows = (SwitchPreference) findPreference(STATUS_BAR_IME_ARROWS);
+        mStatusBarImeArrows.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_IME_ARROWS, 0) == 1);
+        mStatusBarImeArrows.setOnPreferenceChangeListener(this);
+
         updateSettings();
     }
 
@@ -172,6 +180,10 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NAVIGATION_BAR_CAN_MOVE,
                     ((Boolean) newValue) ? 0 : 1);
+            return true;
+        } else if (preference == mStatusBarImeArrows) {
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.STATUS_BAR_IME_ARROWS, (Boolean) objValue ? 1 : 0);
             return true;
         }
         return false;
