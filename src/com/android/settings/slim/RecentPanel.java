89f1a007843609a0cbc24a3912ef20acf43ab0f5
ezio84 <brabus84@gmail.com>
2015-06-12 22:38:21 +0200

    Revert "Revert "Revert "Revert "Revert "Disable search in stock recents: deactivate the switch if slim recents are ON"""""
    This reverts commit e9561ab8efa6b573011df16ac3772f09e8af59ab.

--- src/com/android/settings/slim/RecentPanel.java
+++ src/com/android/settings/slim/RecentPanel.java
@@ -71,8 +71,6 @@ public class RecentPanel extends SettingsPreferenceFragment implements DialogCre
             "recent_card_bg_color";
     private static final String RECENT_CARD_TEXT_COLOR =
             "recent_card_text_color";
-    private static final String RECENTS_SHOW_HIDE_SEARCH_BAR =
-            "recents_show_hide_search_bar";
 
     private SwitchPreference mUseSlimRecents;
     private SwitchPreference mShowRunningTasks;
@@ -84,7 +82,6 @@ public class RecentPanel extends SettingsPreferenceFragment implements DialogCre
     private ColorPickerPreference mRecentPanelBgColor;
     private ColorPickerPreference mRecentCardBgColor;
     private ColorPickerPreference mRecentCardTextColor;
-    private SwitchPreference mDisableStockSearch;
 
     private static final int MENU_RESET = Menu.FIRST;
     private static final int DEFAULT_BACKGROUND_COLOR = 0x00ffffff;
@@ -100,7 +97,6 @@ public class RecentPanel extends SettingsPreferenceFragment implements DialogCre
         if (preference == mUseSlimRecents) {
             Settings.System.putInt(getContentResolver(), Settings.System.USE_SLIM_RECENTS,
                     ((Boolean) newValue) ? 1 : 0);
-            updateRecentsPreferences((Boolean) newValue);
             return true;
         } else if (preference == mShowRunningTasks) {
             Settings.System.putInt(getContentResolver(), Settings.System.RECENT_SHOW_RUNNING_TASKS,
@@ -165,11 +161,6 @@ public class RecentPanel extends SettingsPreferenceFragment implements DialogCre
                     Settings.System.RECENT_PANEL_SHOW_TOPMOST,
                     ((Boolean) newValue) ? 1 : 0);
             return true;
-        } else if (preference == mDisableStockSearch) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR,
-                    ((Boolean) newValue) ? 1 : 0);
-            return true;
         } else if (preference == mMaxApps) {
             int value = Integer.parseInt((String) newValue);
             Settings.System.putInt(getContentResolver(),
@@ -177,10 +168,6 @@ public class RecentPanel extends SettingsPreferenceFragment implements DialogCre
             return true;
         }
         return false;
-    }
-
-    private void updateRecentsPreferences(boolean show) {
-        mDisableStockSearch.setEnabled(!show);
     }
 
     @Override
@@ -250,10 +237,7 @@ public class RecentPanel extends SettingsPreferenceFragment implements DialogCre
     }
 
     private void initializeAllPreferences() {
-        boolean useSlimRecents = Settings.System.getInt(getContentResolver(),
-                                      Settings.System.USE_SLIM_RECENTS, 0) == 1;
         mUseSlimRecents = (SwitchPreference) findPreference(USE_SLIM_RECENTS);
-        mUseSlimRecents.setChecked(useSlimRecents);
         mUseSlimRecents.setOnPreferenceChangeListener(this);
 
         mShowRunningTasks = (SwitchPreference) findPreference(ONLY_SHOW_RUNNING_TASKS);
@@ -266,12 +250,6 @@ public class RecentPanel extends SettingsPreferenceFragment implements DialogCre
                 Settings.System.RECENTS_MAX_APPS, ActivityManager.getMaxRecentTasksStatic(),
                 UserHandle.USER_CURRENT) - 5);
         mMaxApps.disablePercentageValue(true);
-
-        boolean enableDisableStockSearch = Settings.System.getInt(getContentResolver(),
-                                      Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 0) == 1;
-        mDisableStockSearch = (SwitchPreference) findPreference(RECENTS_SHOW_HIDE_SEARCH_BAR);
-        mDisableStockSearch.setChecked(enableDisableStockSearch);
-        mDisableStockSearch.setOnPreferenceChangeListener(this);
 
         // Recent panel background color
         mRecentPanelBgColor =
@@ -340,8 +318,6 @@ public class RecentPanel extends SettingsPreferenceFragment implements DialogCre
         mRecentPanelExpandedMode =
                 (ListPreference) findPreference(RECENT_PANEL_EXPANDED_MODE);
         mRecentPanelExpandedMode.setOnPreferenceChangeListener(this);
-
-        updateRecentsPreferences(useSlimRecents);
     }
 
 }
